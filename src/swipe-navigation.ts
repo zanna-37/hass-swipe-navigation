1b921403b1e02f8894188eb7e6d3dd3be029949c
Zanna_37 <>
2024-02-17 13:59:36 +0000

    refactor(core): extract swipe-exceptions to its own file

--- src/swipe-navigation.ts
+++ src/swipe-navigation.ts
@@ -1,90 +1,10 @@
 import { z } from "zod";
 import { LogLevel, Logger } from "./logger";
+import { exceptions } from "./swipeExceptions";
 
 
 // Console tag
 console.info("%c↔️ Swipe navigation ↔️ - VERSION_PLACEHOLDER", "color: #2980b9; font-weight: 700;");
-
-
-/**
- * Ignore swipes when initiated on elements that match at least one of these CSS selectors.
- *
- * Learn more on CSS selectors
- * [here](https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Selectors).
- */
-const exceptions = [
-
-  // INTERNALS
-  // 💡 Please keep this list sorted alphabetically. Consider the selector as the key after removing
-  // all symbols. Only consider letters and numbers.
-
-  // Dashboard tabs
-  "ha-tabs",
-  // Map
-  "hui-map-card",
-  // Scrollbar
-  ".ha-scrollbar",
-  // Sidebar (contains dashboards)
-  "ha-sidebar",
-  // Slider
-  "ha-slider", // Deprecated since HA v2023.11
-  "input[type=range]",
-  // Tile slider
-  "#slider",
-
-
-  // THIRD PARTIES
-  // 💡 Please keep this list sorted alphabetically. Consider the selector as the key after removing
-  // all symbols. Only consider letters and numbers.
-
-  // UI Card for Better Thermostat (https://github.com/KartoffelToby/better-thermostat-ui-card)
-  "better-thermostat-ui-card",
-  // Big Slider Card (https://github.com/nicufarmache/lovelace-big-slider-card)
-  "big-slider-card",
-  // floor3d-card aka Your Home Digital Twin (https://github.com/adizanni/floor3d-card)
-  "floor3d-card",
-  // Gallery Card (https://github.com/TarheelGrad1998/gallery-card)
-  "gallery-card",
-  // ApexCharts Card by RomRider (https://github.com/RomRider/apexcharts-card)
-  "#graph-wrapper svg.apexcharts-svg",
-  // History explorer card (https://github.com/alexarch21/history-explorer-card)
-  "history-explorer-card",
-  // Bubble Card horizontal buttons stack (https://github.com/Clooos/Bubble-Card)
-  ".horizontal-buttons-stack-container",
-  // my-cards (https://github.com/AnthonMS/my-cards)
-  "my-slider",
-  "my-slider-v2",
-  // @material/mwc-tab-bar (https://www.npmjs.com/package/@material/mwc-tab-bar)
-  //   Used by: Tabbed Card (https://github.com/kinghat/tabbed-card)
-  "mwc-tab-bar",
-  // Plotly Graph Card (https://github.com/dbuezas/lovelace-plotly-graph-card)
-  "#plotly g.draglayer",
-  // Bubble Card button slider (https://github.com/Clooos/Bubble-Card)
-  ".range-slider",
-  // round-slider (https://github.com/thomasloven/round-slider)
-  "round-slider",
-  // Sankey Chart Card (https://github.com/MindFreeze/ha-sankey-chart)
-  "sankey-chart",
-  // Slider button card (https://github.com/mattieha/slider-button-card)
-  "slider-button-card",
-  // Swipe Card (https://github.com/bramkragten/swipe-card)
-  "swipe-card",
-  // Meteoalarm Card (https://github.com/MrBartusek/MeteoalarmCard)
-  ".swiper",
-  // Android TV Card touchpad (https://github.com/Nerwyn/android-tv-card)
-  "toucharea",
-  // Lovelace Vacuum Map card (https://github.com/PiotrMachowski/lovelace-xiaomi-vacuum-map-card)
-  "xiaomi-vacuum-map-card",
-
-
-  // DEPRECATED
-  // will be removed in December 2023
-
-  // 🍄 Mushroom (https://github.com/piitaya/lovelace-mushroom)
-  "mushroom-slider", // it uses the same id as built-in slider
-  // Slider bar (used by the Tile card)
-  "ha-bar-slider", // it uses the same id as built-in slider
-].join(",");
 
 
 const LOG_TAG = "↔️ Swipe navigation:";
