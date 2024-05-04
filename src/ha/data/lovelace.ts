51d9efeaf62125541f379c3cd84a2c4acaa55dcd
Zanna_37 <>
2024-05-04 12:54:10 +0000

    feat(core): add home assistant types

---
+++ src/ha/data/lovelace.ts
@@ -0,0 +1,44 @@
+export interface PanelLovelace {
+  route?: Route;
+  lovelace?: Lovelace;
+}
+
+export interface Route {
+  prefix?: string,
+  path?: string,
+}
+
+export interface Lovelace {
+  config?: LovelaceConfig;
+  // If not set, a strategy was used to generate everything
+  rawConfig?: LovelaceConfig;
+  editMode?: boolean;
+  urlPath?: string | null;
+  mode?: "generated" | "yaml" | "storage";
+  locale?: unknown;
+  enableFullEditMode?: () => void;
+  setEditMode?: (editMode: boolean) => void;
+  saveConfig?: (newConfig: LovelaceConfig) => Promise<void>;
+  deleteConfig?: () => Promise<void>;
+}
+
+export interface LovelaceConfig {
+  title?: string;
+  strategy?: unknown;
+  views?: LovelaceViewConfig[];
+  background?: string;
+}
+
+export interface LovelaceViewConfig {
+  index?: number,
+  title?: string,
+  type?: string,
+  strategy?: unknown,
+  cards?: unknown,
+  path?: string,
+  icon?: string,
+  theme?: string,
+  panel?: boolean,
+  background?: string,
+  visible?: unknown,
+}
