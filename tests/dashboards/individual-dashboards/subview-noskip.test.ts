5c0fc95588848e55da8b09080b05b4ee1f201018
Zanna_37 <>
2024-05-07 19:48:23 +0000

    test(core): add tests for subviews and hidden views

---
+++ tests/dashboards/individual-dashboards/subview-noskip.test.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+import { SwipeHelper } from "../../helpers/touchHelpers";
+
+test("shouldn't skip subview", async ({ page }) => {
+  const dashboardPath = "/subview-noskip";
+  await page.goto(dashboardPath);
+  await expect(page).toHaveURL(dashboardPath + "/0");
+
+  const haAppLayout = page.locator("div[id='view']");
+
+  await SwipeHelper.swipeLeft(haAppLayout);
+  await expect(page).toHaveURL(dashboardPath + "/1");
+  await SwipeHelper.swipeLeft(haAppLayout);
+  await expect(page).toHaveURL(dashboardPath + "/2");
+
+  await SwipeHelper.swipeRight(haAppLayout);
+  await expect(page).toHaveURL(dashboardPath + "/1");
+  await SwipeHelper.swipeRight(haAppLayout);
+  await expect(page).toHaveURL(dashboardPath + "/0");
+});
