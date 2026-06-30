f68ddaaa263408619ea966a589a0153427f75aa7
Martin Kopecky <33658071+martkopecky@users.noreply.github.com>
2026-06-30 16:07:17 +0200

    fix(swipe-exception): add 'Weather Forecast Card' to the swipe exception list

--- src/swipeExceptions.ts
+++ src/swipeExceptions.ts
@@ -119,6 +119,9 @@ const exceptions: ExceptionEntry[] = [
   "vehicle-info-card",
   // Vehicle Status Card (https://github.com/ngocjohn/vehicle-status-card)
   "vehicle-status-card",
+  // Weather Forecast Card (https://github.com/troinine/ha-weather-forecast-card)
+  { selector: ".is-scrollable",
+    host: "weather-forecast-card" },
   // Lovelace Vacuum Map card (https://github.com/PiotrMachowski/lovelace-xiaomi-vacuum-map-card)
   "xiaomi-vacuum-map-card",
   // CSS-Swipe-Card (https://github.com/Nemuritor01/css-swipe-card)
